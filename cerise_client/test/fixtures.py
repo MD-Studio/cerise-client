d320e2bbb0f37d8fbde7c07658d20819b4d3e35e
Lourens Veen <l.veen@esciencecenter.nl>
2017-10-10 17:38:43 +0200

    Rework API and prepare to support unmanaged services

--- cerise_client/test/fixtures.py
+++ cerise_client/test/fixtures.py
@@ -1,6 +1,8 @@
 import docker
 import os
 import pytest
+import requests
+import time
 
 import cerise_client.service as cs
 
@@ -27,7 +29,7 @@ def test_image(request):
 def test_service(request, test_image):
     from .clean_up import clean_up_service
     clean_up_service('cerise_client_test_service')
-    srv = cs.create_service('cerise_client_test_service', 29593,
+    srv = cs.create_managed_service('cerise_client_test_service', 29593,
             test_image)
 
     yield srv
@@ -37,4 +39,21 @@ def test_service(request, test_image):
 @pytest.fixture()
 def this_dir(request):
     return os.path.dirname(__file__)
+
+def create_test_job(test_service, this_dir, name):
+    job = test_service.create_job(name)
+    r = requests.get('http://localhost:29593/files/input/' + name + '/')
+    assert r.status_code == 200
+
+    # run job to create some outputs
+    job.set_workflow(os.path.join(this_dir, 'test_workflow2.cwl'))
+    job.add_input_file('input_file', os.path.join(this_dir, 'test_workflow2.cwl'))
+    job.run()
+    while job.is_running():
+        time.sleep(0.1)
+    assert job.state == 'Success'
+
+    counts = job.outputs['counts']
+    assert counts.text != ''
+    return job
 

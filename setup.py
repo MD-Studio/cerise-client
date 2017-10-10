0431f02190ace5ff78a5dec77ab48c74dff69827
Lourens Veen <l.veen@esciencecenter.nl>
2017-10-10 17:41:21 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
         name = 'cerise_client',
         packages = ['cerise_client'],
-        version = '0.1.0',
+        version = '0.2.0',
         description = 'Client library for the Cerise CWL job running service',
         author = 'Lourens Veen',
         author_email = 'l.veen@esciencecenter.nl',

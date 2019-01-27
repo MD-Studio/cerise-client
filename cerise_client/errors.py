758db32be18e58ef2ac5e576c508a41be4ea2c7b
Lourens Veen <l.veen@esciencecenter.nl>
2019-01-27 18:12:21 +0100

    Remove now-unused errors, see Cerise Manager

--- cerise_client/errors.py
+++ cerise_client/errors.py
@@ -1,27 +1,3 @@
-class ServiceNotFound(Exception):
-    """
-    The given service does not exist on this machine. Either it was
-    never created, or it was destroyed, or the Docker container running
-    it was destroyed by an outside influence.
-    """
-    pass
-
-class ServiceAlreadyExists(Exception):
-    """
-    A service with the given name already exists on this machine, so
-    another one cannot be created.
-    """
-    pass
-
-class PortNotAvailable(Exception):
-    """
-    The requested port is occupied by another service, or by some
-    other program that has bound it on localhost. Either stop the
-    blocking service or program, or try again with a different
-    port number.
-    """
-    pass
-
 class JobNotFound(Exception):
     """
     The given job does not exist on this service. Either it never

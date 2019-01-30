3e8cd931c5f3b5eb57677ce8a2aa0178cd3568b6
Lourens Veen <l.veen@esciencecenter.nl>
2019-01-30 18:40:11 +0100

    Remove py-docker from requirements, upgrade to v3 for tests

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
         license = 'Apache License 2.0',
         python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
         install_requires=[
-                'docker>=2.3.0,<3',
                 'defusedxml',
                 'future',
                 'requests'
